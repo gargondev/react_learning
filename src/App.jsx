36356ada2c8645b2b9e9051e908d7c35bdd8e2f4
heldeveloper <heliezer.developer@gmail.com>
2022-05-12 15:27:53 -0300

    Aplication Base

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Lista from './components/Lista'
 
 
 function App() {
     return (
         <div>
             <h1>Ola Mundo --Teste</h1>
-            <Lista></Lista>
         </div>
     )
 }
