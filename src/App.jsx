8fd821f6efc1583bea05fa8776640f6801107f9d
heldeveloper <heliezer.developer@gmail.com>
2022-05-12 14:04:15 -0300

    First Aplication App React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Lista from './components/Lista'
+
+
+function App() {
+    return (
+        <div>
+            <h1>Ola Mundo --Teste</h1>
+            <Lista></Lista>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Lista.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Lista(){
+    return (
+       <img src={require("./img/logo-topo.png")} alt="" srcset="" />
+    );
+}
+
+
+export default Lista
